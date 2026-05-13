0a8a78cdb1c3eb41523e31b8ca8c8b3aeef6651c
Sreeram Andra <vssa8989@bu.edu>
2026-05-13 14:27:41 +0100

    changes

--- QuantFinanceProject/earnings_agent/storage/config.py
+++
@@ -1,6 +0,0 @@
-# earnings_agent/storage/config.py
-import os
-
-# This centralizes the schema name so other modules can import it
-# without creating circular dependencies.
-DB_SCHEMA = os.getenv("EARNINGS_DB_SCHEMA", "earnings_data")
